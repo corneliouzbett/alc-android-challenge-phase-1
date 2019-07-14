a61cdfae28825ce199870179bc93a70c1cc0a604
corneliouz Bett <kbett68@gmail.com>
2019-07-14 11:23:06 +0300

    Initial commit

---
+++ app/src/main/java/com/corneliouzbett/alc_android_challenge_phase_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.corneliouzbett.alc_android_challenge_phase_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
